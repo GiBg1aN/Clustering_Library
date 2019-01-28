ea43ead4afbc1177a671bbd7a0da891512576cc8
Gianluca Bigaglia <bigagliagianluca@gmail.com>
2019-01-28 18:37:56 +0100

    Fix eigenvector-based clustering bugs and complete implementation
    The main bug cause was that the return type of np.linalg.eig function
    is column based but was used as row based.
    
    Co-authored-by: hjorvardr <lorenzo.giudice92@gmail.com>

--- eigenvector_based.py
+++ eigenvector_based.py
@@ -1,70 +1,45 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import networkx as nx
+import numpy as np
 import seaborn as sns
-from sklearn import datasets, cluster
-from sklearn.neighbors import kneighbors_graph, radius_neighbors_graph
-from sklearn.preprocessing import StandardScaler
 
 from itertools import cycle, islice
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import pairwise_distances
+from sklearn.metrics.pairwise import rbf_kernel
 
 
+
+# DATASET GENERATION AND PREPARATION
 np.random.seed(0)
-
-# GENERATE DATASET
 n_samples = 1500
+
 blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
-
+# varied = datasets.make_blobs(n_samples=n_samples, cluster_std=[1.0, 2.5, 0.5], random_state=170)
 X, y = blobs
-# normalize dataset for easier parameter selection
-X = StandardScaler().fit_transform(X)
+X = StandardScaler().fit_transform(X) # normalize dataset for easier parameter selection
+D = pairwise_distances(X) # Distance matrix as euclidean distance
 
-# use 10-nn to generate adjacency matrix
-# A = kneighbors_graph(X, n_neighbors=10, mode='distance', include_self=False)
-A = radius_neighbors_graph(X, 0.3, mode='distance', include_self=False)
-
-# PLOTTING
-plt.figure(0)
-plt.subplot(2, 2, 1)
-plt.scatter(X[:, 0], X[:, 1], s=10)
-plt.grid()
-
-G = nx.Graph()
-for node in range(len(X)):
-    G.add_node(node)
-
-G.add_edges_from(nx.from_scipy_sparse_matrix(A).edges())
-plt.subplot(2, 2, 2)
-nx.draw_networkx(G, [(x, y) for x, y in X], node_size=15, with_labels=False)
-plt.grid()
-
-
-# COMPUTE EIGENVALUES-EIGENVECTORS
-A = A.toarray()
-
-plt.subplot(2, 2, 3)
-sns.heatmap(np.corrcoef(A), square = True)
-
+mult = 42
+gamma = 1 / (mult * np.var(D))
+A = rbf_kernel(D, gamma=gamma) # Gaussian distance function
+    
+# Compute eigenvalues-eigenvectors
 evals, evects = np.linalg.eig(A)
-
-idx = evals.argsort()[::-1]   
-evals = evals[idx]
-evects = evects[:,idx]
+evects = evects.T # from column to row vectors
 
 
 # CLUSTERING
-threshold = 0.001
-bigger_eig_id = 0
-clusters = []
-continue_clustering = True
-
-
-def eigenvector_thresholding(evect, threshold):
+def eigenvector_thresholding(evect, threshold, clustered_elements):
     clustered_idx = []
+    new_clustered_elements = clustered_elements.copy()
     for i in range(len(evect)):
-        if evect[i] > threshold:
+        if i not in clustered_elements and abs(evect[i]) > threshold:
             clustered_idx.append(i)
-    return clustered_idx
+            new_clustered_elements.append(i)
+    return clustered_idx, new_clustered_elements
+
 
 def remove_clustered(evect, clustered_idx):
     new_evect = evect.copy()
@@ -72,23 +47,80 @@ def remove_clustered(evect, clustered_idx):
         new_evect[x] = 0 
     return new_evect
 
+
+def generate_cluster_matrix(A, clusters):
+    res = np.zeros_like(A)
+    supp = []
+    for i in range(len(clusters)):
+        for j in range(len(clusters[i])):
+            supp.append(clusters[i][j])
+
+    for i in range(len(supp)):
+        for j in range(len(supp)):
+            res[i, j] = A[supp[i], supp[j]]
+    return res
+
+
+threshold = 0.001
+bigger_eig_id = 0
+clusters = []
+clustered_elements = []
+continue_clustering = True
+
 while(continue_clustering):
-    clusters.append(eigenvector_thresholding(evects[bigger_eig_id], threshold))
-    if clusters[bigger_eig_id] == []:
+    if np.amax(evals) == -float("inf"):
+        break;
+    bigger_eig_id = np.argmax(evals)
+    cluster, clustered_elements = eigenvector_thresholding(evects[bigger_eig_id], threshold, clustered_elements)
+    clusters.append(cluster)
+    if len(clustered_elements) == n_samples:
         continue_clustering = False
-    else:
-        bigger_eig_id += 1
-        for clustered_idx in clusters:
-            evects[bigger_eig_id] = remove_clustered(evects[bigger_eig_id], clustered_idx)
+    evals[bigger_eig_id] = -float("inf")
 
-print(bigger_eig_id, "clusters found")
+print(len(clusters), "clusters found")
 
-colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a',
-                                             '#f781bf', '#a65628', '#984ea3',
-                                             '#999999', '#e41a1c', '#dede00']),
-                                      int(bigger_eig_id + 1))))
-plt.subplot(2, 2, 4)
+
+# PLOTTING
+colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a', '#f781bf', '#a65628',
+                                    '#984ea3', '#999999', '#e41a1c', '#dede00']),
+                              int(len(clusters)+ 1))))
+symbols = np.array(list(islice(cycle(["*","x","+","o",".","^","<",">","P","p","X","D","d"]),
+                              len(clusters) + 1)))
+
+plt.figure(0)
+plt.subplot(2, 3, 1)
+plt.scatter(X[:, 0], X[:, 1], s=10)
+plt.grid()
+plt.title("Original Data")
+
+G = nx.Graph()
+for node in range(len(X)):
+    G.add_node(node)
+
+G.add_edges_from(nx.from_numpy_array(A).edges())
+plt.subplot(2, 3, 2)
+nx.draw_networkx(G, [(x, y) for x, y in X], node_size=15, with_labels=False)
+plt.grid()
+plt.title("Graph Data")
+
+plt.subplot(2, 3, 3)
+plt.title("Affinity matrix")
+sns.heatmap(A, square = True, cbar_kws={"shrink": .2})
+
+plt.subplot(2, 3, 4)
 plt.grid()
 for i in range(len(clusters)):
-    plt.scatter(X[clusters[i], 0], X[clusters[i], 1], s=10, color=colors[i])
+    plt.scatter(X[clusters[i], 0], X[clusters[i], 1], s=90, color=colors[i], marker=symbols[i])
+plt.title("Clustered data")
+
+plt.subplot(2, 3, 5)
+plt.scatter(list(range(len(evals))), evals, s=10)
+plt.grid()
+plt.title("Eigenvalues")
+
+plt.subplot(2, 3, 6)
+plt.title("Clusters affinity matrix")
+sns.heatmap(generate_cluster_matrix(A, clusters), square = True, cbar_kws={"shrink": .2})
+
 plt.show()
+
